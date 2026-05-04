22ba80f801e12108821e3dac9d063d92ee0c2538
Md <mdc6@nycstudents.net>
2026-05-04 02:27:11 -0400

    commit 2, added category select

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("App initialized and files linked successfully.");
+// Global Selectors
+const loginBtn = document.getElementById('login-btn');
+const logoutBtn = document.getElementById('logout-btn');
+const findMealBtn = document.getElementById('find-meal-btn');
+const mealCategorySelect = document.getElementById('meal-category');
+const recipeTitle = document.getElementById('recipe-title');
+
+console.log("Structure setup: Category select and Recipe display");
