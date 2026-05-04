ec460e00b6f2f07e5ecb7ca5c48d1d82ecd5fafd
Md <mdc6@nycstudents.net>
2026-05-04 03:19:10 -0400

    removed old code

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const logoutBtn = document.getElementById('logout-btn');
 const findMealBtn = document.getElementById('find-meal-btn');
 const mealCategorySelect = document.getElementById('meal-category');
 const recipeTitle = document.getElementById('recipe-title');
-
-console.log("UI components styled");
 const favBtn = document.getElementById('fav-btn');
 
 // Login and Modal Logic
